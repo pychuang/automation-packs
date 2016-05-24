29d594c98509cf2c7ccb26e3646e71c50410edda
Po-Yu Chuang <t-pochua@microsoft.com>
2016-05-23 18:17:25 -0700

    Runbooks/TestConnection.ps1

---
+++ 101-get-vm-tutorial/Runbooks/TestConnection.ps1
@@ -0,0 +1,8 @@
+﻿workflow TestConnection
+{
+	$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+    Write-Output $Conn
+	Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
+    -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+    Write-Output "TestConnection Finished"
+}
