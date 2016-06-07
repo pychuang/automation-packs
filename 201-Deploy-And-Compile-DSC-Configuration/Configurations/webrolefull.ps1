7d43252203b04f78b2636cce372cfe4253135c7d
Po-Yu Chuang <t-pochua@microsoft.com>
2016-06-07 14:44:15 -0700

    Configurations/webrolefull.ps1: enable smtp

--- 201-Deploy-And-Compile-DSC-Configuration/Configurations/webrolefull.ps1
+++ 201-Deploy-And-Compile-DSC-Configuration/Configurations/webrolefull.ps1
@@ -2,40 +2,82 @@ Configuration webrolefull {
 
     $features = @(
         @{Name = "SMTP-Server"; Ensure = "Present"},
-        @{Name = "Web-WMI"; Ensure = "Present"},
-        @{Name = "Web-Server"; Ensure = "Present"},
-        @{Name = "Web-WebServer"; Ensure = "Present"},
-        @{Name = "Web-Common-http"; Ensure = "Present"},
-        @{Name = "Web-Default-Doc"; Ensure = "Present"},
-        @{Name = "Web-Dir-Browsing"; Ensure = "Present"},
-        @{Name = "Web-Http-Errors"; Ensure = "Present"},
-        @{Name = "Web-Static-Content"; Ensure = "Present"},
-        @{Name = "Web-Health"; Ensure = "Present"},
-        @{Name = "Web-Http-Logging"; Ensure = "Present"},
-        @{Name = "Web-Performance"; Ensure = "Present"},
-        @{Name = "Web-Stat-Compression"; Ensure = "Present"},
-        @{Name = "Web-Dyn-Compression"; Ensure = "Present"},
-        @{Name = "Web-Security"; Ensure = "Present"},
-        @{Name = "Web-Filtering"; Ensure = "Present"},
-        @{Name = "Web-Basic-Auth"; Ensure = "Present"},
-        @{Name = "Web-Windows-Auth"; Ensure = "Present"},
-        @{Name = "Web-App-Dev"; Ensure = "Present"},
-        @{Name = "Web-Net-Ext45"; Ensure = "Present"},
-        @{Name = "Web-Asp-Net45"; Ensure = "Present"},
-        @{Name = "Web-ISAPI-Ext"; Ensure = "Present"},
-        @{Name = "Web-ISAPI-Filter"; Ensure = "Present"},
-        @{Name = "Web-Ftp-Server"; Ensure = "Present"},
-        @{Name = "Web-Mgmt-Tools"; Ensure = "Present"},
-        @{Name = "Web-Mgmt-Console"; Ensure = "Present"}     
+        @{Name = "Web-WMI"; Ensure = "Present"}
        )
 
     node full {
-    
+
+        function Install-Prerequisites ()
+        {
+            Add-WindowsFeature SMTP-Server
+            Add-WindowsFeature Web-WMI
+
+            #Add-PSSnapin WebAdministration
+        }
+
+        # https://blogs.technet.microsoft.com/bspieth/2013/02/18/configure-the-iis-6-smtp-server-with-wmi-and-powershell/
+        function Configure-SMTPService ()
+        {
+            Write-Host -Foregroundcolor White " -> Changing the start-up type of SMTP service to 'Automatic'..."
+            Set-Service "SMTPSVC" -StartupType Automatic -ErrorAction SilentlyContinue
+            if ($?)
+            {
+                Write-Host -Foregroundcolor Green " [OK] Successfully changed startup type."
+            }
+            else
+            {
+                Write-Host -Foregroundcolor Red " [Error] Unable to change startup type."
+                throw
+            }
+
+            Write-Host -Foregroundcolor White " -> Starting SMTP service..."
+            Start-Service "SMTPSVC" -ErrorAction SilentlyContinue
+            if ($?)
+            {
+                Write-Host -Foregroundcolor Green " [OK] Service successfully started."
+            }
+            else
+            {
+                Write-Host -Foregroundcolor Red " [Error] Unable to start service."
+                throw
+            }
+
+            Write-Host -Foregroundcolor White " -> Configuring virtual SMTP server..."
+            try
+            {
+                # For IISSmtpServerSetting, refer https://msdn.microsoft.com/en-us/library/ms526057(v=vs.90).aspx
+                $virtualSMTPServer = Get-WmiObject IISSmtpServerSetting -namespace "root\MicrosoftIISv2" | Where-Object { $_.name -like "SmtpSVC/1" }
+
+                # The "Only the list below" or "All except the list below" choice is controlled by $virtualSMTPServer.RelayIpList,
+                # but its format is not straightforward.  We use a simpler setting here.
+
+                # Anonymous access
+                #$virtualSMTPServer.AuthAnonymous = $true
+
+                # Integrated Windows Authentication
+                $virtualSMTPServer.AuthNTLM = $true
+
+                # Allow all computers which successfully authenticate to relay, regardless of the list above
+                $virtualSMTPServer.RelayForAuth = -1
+
+                $virtualSMTPServer.Put() | Out-Null
+                Write-Host -Foregroundcolor Green " [OK] Successfully configured virtual SMTP server."
+            }
+            catch
+            {
+                Write-Host -Foregroundcolor Red " [Error] Unable to configure virtual SMTP server."
+                throw
+            }
+        }
+
         foreach ($feature in $features){
             WindowsFeature ($feature.Name) {
                 Name = $feature.Name
                 Ensure = $feature.Ensure
             }
         }
+
+        Set-ExecutionPolicy RemoteSigned
+        Configure-SMTPService
     }
 }
